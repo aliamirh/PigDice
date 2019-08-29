7f2379dbc0510c9b41e6dc91b2d2ea8d7f346e6b
Ali Amirhamzeh and Peterson <ali@>
2019-08-29 11:49:54 -0700

    adds player2 and shows winning alert

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,13 @@ var randomNumber = function() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+var winCon  = function() {if(player1.totalScore >=100){
+    alert("you fuckin wonnnnnnn!");
+  } else if (player2.totalScore >=100){
+    alert("you won!");
+  }
+}
+
 function Player(turnScore, totalScore, roll) {
 
   this.turnScore = 0;
@@ -35,14 +42,19 @@ Player.prototype.hold = function (turnScore) {
 
 
 ///////// user interface ////////
-// var totalScore = ;
+
 
 
 
 $(function() {
 
-  var turnScore  = 0;
+  var turnScore1  = 0;
   var total1 = 0
+  var turnScore2  = 0;
+  var total2 = 0
+
+//////////// player 1 ///////////////
+
 
   $("#rollBtn1").click(function(event) {
     event.preventDefault();
@@ -54,10 +66,34 @@ $(function() {
     $("#turnOut1").text("Turn Score: " + parseInt(player1.turnScore));
   });
 
+
+
   $("#holdBtn1").click(function(event) {
     event.preventDefault();
     player1.hold(player1.turnScore);
 
     $("#totalScore1").text("Player one score: " + parseInt(player1.totalScore));
+    winCon();
+  });
+
+
+///////////// Player 2 //////////////
+
+  $("#rollBtn2").click(function(event) {
+    event.preventDefault();
+
+    player2.roll = parseInt(randomNumber());
+    player2.rollone(player2.roll);
+
+    $("#rollOut2").text("Current Roll: " + parseInt(player2.roll));
+    $("#turnOut2").text("Turn Score: " + parseInt(player2.turnScore));
+  });
+
+  $("#holdBtn2").click(function(event) {
+    event.preventDefault();
+    player2.hold(player2.turnScore);
+
+    $("#totalScore2").text("Player two score: " + parseInt(player2.totalScore));
+    winCon();
   });
 });
